de2a617e4246f1bcf37998841b915efe59fbee47
Marcos Paulo <mapazerogp@gmail.com>
2018-11-05 23:14:21 -0200

    Commit inicial

---
+++ Sokoban/cmake-build-debug/CMakeFiles/Sokoban.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sokoban.dir/main.c.o"
+  "Sokoban.pdb"
+  "Sokoban"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sokoban.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Sokoban/main.c
@@ -0,0 +1,37 @@
+/*
+ * Projeto de Computação I: Sokoban em C. 11/2018
+ *
+ * Marcos  Paulo Paolino e Marco  Cabral Campelo
+ *
+ * Referência:
+ *
+ * 0 = Vazio
+ * 1 = Parede
+ * 2 = Caixa
+ * 3 = DestinoCaixa
+ * 4 = Jogador
+ */
+
+#include <stdio.h>
+#define True 1
+#define False 0
+int main() {
+    menu();
+    return 0;
+}
+
+int menu() {
+    printf("███████╗ ██████╗ ██╗  ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗\n");
+    printf("██╔════╝██╔═══██╗██║ ██╔╝██╔═══██╗██╔══██╗██╔══██╗████╗  ██║\n");
+    printf("███████╗██║   ██║█████╔╝ ██║   ██║██████╔╝███████║██╔██╗ ██║\n");
+    printf("╚════██║██║   ██║██╔═██╗ ██║   ██║██╔══██╗██╔══██║██║╚██╗██║\n");
+    printf("███████║╚██████╔╝██║  ██╗╚██████╔╝██████╔╝██║  ██║██║ ╚████║\n");
+    printf("╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝\n \n \n");
+    printf("Feito por Marcos Paulo Paolino e Marco Cabral Campelo");
+    printf("\n Selecione a fase: \n ");
+
+}
+
+
+
+##
