b8375bb5d2ddec1a601912ac5953fb3c92d9b3f0
Mapa0 <mapazerogp@gmail.com>
2018-11-07 12:53:11 -0200

    Só mais um, feito hoje no dia 7 de novembro pela manhã.

--- Sokoban/main.c
+++ Sokoban/main.c
@@ -17,96 +17,161 @@
 
 typedef enum { false, true } bool;
 
-int jogo(char fase){
+int exibirMenu() {
 
-    switch (fase) {
+    char fase;
+    printf("\n \n");
+    printf("███████╗ ██████╗ ██╗  ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗\n");
+    printf("██╔════╝██╔═══██╗██║ ██╔╝██╔═══██╗██╔══██╗██╔══██╗████╗  ██║\n");
+    printf("███████╗██║   ██║█████╔╝ ██║   ██║██████╔╝███████║██╔██╗ ██║\n");
+    printf("╚════██║██║   ██║██╔═██╗ ██║   ██║██╔══██╗██╔══██║██║╚██╗██║\n");
+    printf("███████║╚██████╔╝██║  ██╗╚██████╔╝██████╔╝██║  ██║██║ ╚████║\n");
+    printf("╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝\n \n");
+    printf("Feito por Marcos Paulo Paolino e Marco Cabral Campelo");
+    printf("\n Selecione a fase: \n");
+    printf("1 -> Fase 1 \n2 -> Fase 2 \n3 -> Fase 3 \n0 -> Externo \nq -> Sair do Jogo\n\n");
 
-        case '1': {
-            printf("\n Fase 1: \n");
-            int level[6][6] = {{1, 1, 1, 1, 1, 0},
-                               {1, 4, 0, 0, 1, 1},
-                               {1, 3, 2, 2, 0, 1},
-                               {1, 0, 0, 1, 0, 1},
-                               {1, 0, 0, 0, 0, 0},
-                               {1, 1, 1, 1, 1, 1}};
+    fase = getchar();
 
-            for(int i = 0; i < 6; i++) {
+    if(fase == '0' || fase == '1' || fase == '2' || fase == '3')
+        return fase;
 
-                for (int j = 0; j < 6; j++) {
+    else if (fase == 'q')
+        return -1;
 
-                    switch (level[i][j]) {
-
-                        case 0:
-                            printf(" ");
-                            break;
-                        case 1:
-                            printf("█");
-                            break;
-                        case 2:
-                            printf("#");
-                        case 3:
-                            printf("x");
-                            break;
-                        case 4:
-                            printf("@");
-                            break;
-                        default:
-                            printf(" ");
-
-                    }
-                }
-                printf("\n");
-            }
-            printf("\n\n\n\n\n\n\n\n\n\n\n");
-        }
-        case '2': {
-
-            break;
-        }
-        case '3':
-            break;
-        case '0':
-            break;
+    else {
+        system("clear");
+        printf("Opção inválida, digite novamente! ");
+        return exibirMenu();
     }
-
-    return 0;
 }
 
-
-
-int menu() {
-
-    while(true) {
-        char fase;
-        printf("\n \n");
-        printf("███████╗ ██████╗ ██╗  ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗\n");
-        printf("██╔════╝██╔═══██╗██║ ██╔╝██╔═══██╗██╔══██╗██╔══██╗████╗  ██║\n");
-        printf("███████╗██║   ██║█████╔╝ ██║   ██║██████╔╝███████║██╔██╗ ██║\n");
-        printf("╚════██║██║   ██║██╔═██╗ ██║   ██║██╔══██╗██╔══██║██║╚██╗██║\n");
-        printf("███████║╚██████╔╝██║  ██╗╚██████╔╝██████╔╝██║  ██║██║ ╚████║\n");
-        printf("╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝\n \n");
-        printf("Feito por Marcos Paulo Paolino e Marco Cabral Campelo");
-        printf("\n Selecione a fase: \n");
-        printf("1 -> Fase 1 \n2 -> Fase 2 \n3 -> Fase 3 \n0 -> Externo \nq -> Sair do Jogo\n\n");
-
-        fase = getc(stdin);
-
-        if(fase == '0' || fase == '1' || fase == '2' || fase == '3')
-            jogo(fase);
-
-        else if (fase == 'q')
-            return 0;
-
-        else
+int mostrarMapa(int movel[6][6], const int original[6][6]) {
+    system("clear");
+    int level[6][6];
+    for(int i = 0; i < 6; i++)
+        for(int j = 0; j < 6; j++)
         {
-            system("clear");
-            printf("Opção inválida, digite novamente! ");
+            if(original[i][j] == 3 && movel [i][j] == 0)
+            {
+                level[i][j] = original[i][j];
+            }
+            else level[i][j] = movel[i][j];
         }
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 6; j++) {
+            switch (level[i][j]) {
+
+                case 0:
+                    printf(" ");
+                    break;
+                case 1:
+                    printf("█");
+                    break;
+                case 2:
+                    printf("#");
+                    break;
+                case 3:
+                    printf("x");
+                    break;
+                case 4:
+                    printf("@");
+                    break;
+                default:
+                    printf("?");
+            }
+        }
+        printf("\n");
     }
+}
+
+int jogo(char fase){
+    system("clear");
+    if (fase == 0)
+    {
+        /*fase customizada!*/
+    }
+    else
+    {
+        char movimento;
+        bool cima,baixo,direita,esquerda,rodando=true;
+        int movel[6][6] = {{1, 1, 1, 1, 1, 1},
+                           {1, 0, 0, 0, 0, 1},
+                           {1, 0, 0, 3, 0, 1},
+                           {1, 0, 2, 0, 0, 1},
+                           {1, 0, 0, 4, 0, 1},
+                           {1, 1, 1, 1, 1, 1}};
+
+        const int original[6][6] = {{1, 1, 1, 1, 1, 1},
+                                    {1, 0, 0, 0, 0, 1},
+                                    {1, 0, 0, 3, 0, 1},
+                                    {1, 0, 2, 0, 0, 1},
+                                    {1, 0, 0, 4, 0, 1},
+                                    {1, 1, 1, 1, 1, 1}};
+        while(rodando)
+        {
+        mostrarMapa(movel,original);
+        switch(movimento = getchar()) {
+            case 'w':
+                for(int i = 0; i < 6; i++)
+                    for (int j = 0; j < 6; j++) {
+                        if(movel[i][j] == 4)
+                        {
+                            movel[i-1][j] = 4;
+                            movel[i][j] = 0;
+                        }
+                    }
+                break;
+            case 'a':
+                for(int i = 0; i < 6; i++)
+                    for (int j = 0; j < 6; j++) {
+                        if(movel[i][j] == 4)
+                        {
+                            movel[i][j-1] = 4;
+                            movel[i][j] = 0;
+                        }
+                    }
+                break;
+            case 's':
+                for(int i = 0; i < 6; i++)
+                    for (int j = 0; j < 6; j++) {
+                        if(movel[i][j] == 4)
+                        {
+                            movel[i+1][j] = 4;
+                            movel[i][j] = 0;
+                        }
+                    }
+                break;
+            case 'd':
+                for(int i = 0; i < 6; i++)
+                    for (int j = 0; j < 6; j++) {
+                        if(movel[i][j] == 4)
+                        {
+                            movel[i][j+1] = 4;
+                            movel[i][j] = 0;
+                        }
+                    }
+                break;
+            case 'q':
+                rodando = false;
+                break;
+            default:
+                break;
+            }
+        }
+
+    }
+    return 0;
 }
 
 int main() {
     system("clear");
-    menu();
+    int level = exibirMenu();
+    if(level != -1)
+        jogo(level);
+    else{
+        system("clear");
+        return 0;
+    }
     return 0;
 }
